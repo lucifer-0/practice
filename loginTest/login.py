10a3b17f31bcb49d4f4c76cb762869146e7e6ca9
lucifer-0 <1284526746@qq.com>
2016-10-08 23:43:44 +0800

    login1.0
    完成login.py的练习

--- loginTest/login.py
+++ loginTest/login.py
@@ -1,16 +1,10 @@
 # -*- coding:utf-8 -*-
-import os
+import sys
 
 accout_file = 'accounts.txt'
 lock_file = 'lock.txt'
 
-fp = open("test.txt", 'r+')
-
-userIf = False  #判断是否匹配了用户名
-userTr = False  #判断用户名是否输入正确
-
-passIf = False  #判断是否匹配了密码
-passTr = False  #判断密码是否输入正确
+match_flag = False
 
 count = 0   #记录失败次数
 
@@ -18,36 +12,40 @@ print '欢迎登录！'
 print '请在下方输入用户名和密码'
 
 while True:
-    username = raw_input('请输入用户名：') 
-    password = raw_input('请输入密码：')
+    username = raw_input('请输入用户名：') #当用户输入用户名后检查此用户是否lock
+    lock_check = file(lock_file)
 
+    for line in lock_check.readlines(): #循环lock文件
+        if username in line:
+            lock_check.close()  #在退出之前先关闭打开的文件
+            sys.exit('\033[31;1mUser %s is locked!' % username) #如果locked就直接退出
+
+    lock_check.close()  #文件打开后要关闭
+
+    password = raw_input('请输入密码：')
     print '验证中...'
 
-    for line in fp:
-        if userIf == False:
-            userIf = True
-            passIf = False
-            print line, username, type(line), type(username)
-            if line == username:
-                print 'y'
-                userTr = True
-                passIf = False
-        elif passIf == False:
-            passIf = True
-            userIf = False
-            #print line, password
-            if line == password:
-                passTr = True
+    f = file(accout_file, 'rb')
 
-    if userTr == True & passTr == True:
+    for line in f.readlines():
+        user, passwd = line.strip('\n').split()
+        #去掉每行多余的\n，并把这一行按空格分成两列，分别赋值为user, passwd两个变量
+
+        if username == user and password == passwd:
+            print 'Match!', username
+            match_flag = True
+            f.close()
+            break
+
+    if match_flag:
         print '登录成功！'
         break
     else:
         print '用户名或密码不正确！'
-        print userTr, passTr
         count += 1
         if count == 3:
+            lock_c = file(lock_file, 'ab')
+            lock_c.write('\n' + username)
             print '错误次数达到3次，该账号已锁定'
             break
 
-fp.close()
--- loginTest/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-user = '123'
-line = '123'
-
-print type(user), type(line)
-
-if line == user:
-    print 'y'
